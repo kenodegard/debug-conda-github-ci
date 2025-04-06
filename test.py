3a8075d88fd10c47aa4e7b0e55762e3b7ecee65f
Jan Janßen <janssen@mpie.de>
2025-04-06 13:54:44 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+from conda.base.context import context
+
+
+if __name__ == "__main__":
+    env_name = "py313"
+    env_path = os.path.join(context.root_prefix, "..", env_name)
+    output = check_output(
+        ["conda", "run", "-p", env_path, "which", "python"], 
+        universal_newlines=True,
+    )
+    print(output)
