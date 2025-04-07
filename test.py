8827373f5257c810d9c831240e0c97079ac6b664
Ken Odegard <kodegard@anaconda.com>
2025-04-07 11:00:35 -0600

    View temp script

--- test.py
+++ test.py
@@ -1,19 +1,37 @@
 import os
-from subprocess import check_output
+from subprocess import run, PIPE, check_output
 from conda.base.context import context
 import sys
+import re
+from pathlib import Path
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     env_name = "py313"
     env_path = os.path.join(context.root_prefix, "..", env_name)
-    output = check_output(
+    info = check_output(
         ["conda", "info"],
         universal_newlines=True,
     )
-    print(output, file=sys.stderr)
-    output = check_output(
-        ["conda", "run", "-p", env_path, "which", "python"], 
+    print(info, file=sys.stderr)
+
+    output = run(
+        ["conda", "run", "-p", env_path, "which", "python"],
         universal_newlines=True,
+        env={
+            **os.environ,
+            "CONDA_TEST_SAVE_TEMPS": "true",
+        },
+        check=True,
+        stdout=PIPE,
+        stderr=PIPE,
     )
-    print(output)
+    stdout = output.stdout
+    stderr = output.stderr
+    print(stdout)
+
+    m = re.search(r"CONDA_TEST_SAVE_TEMPS :: .+ (\S+)\n", stderr)
+    if m:
+        batch = m.group(1)
+        print(f"Batch file: {batch}", file=sys.stderr)
+        print(Path(batch).read_text(), file=sys.stderr)
